4312d4b4a3fa1bfd5077421c49fae9845d44ca67
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-09-25 22:46:08 +0300

    HOME (with Load MORE)+Move search (w/o Load MORE)+id+modal template

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,22 +1,27 @@
 export default function getRefs() {
   return {
-    //todo Получаем ссылку на input form:
+    //! Получаем ссылку на input form:
     searchForm: document.querySelector('#search-form'),
 
-    //todo Получаем ссылку на div-контейнер для разметки карточек изображений:
+    //! Получаем ссылку на div-контейнер для разметки карточек изображений:
     imageCards: document.querySelector('.gallery'),
 
-    //todo Получаем ссылку на кнопку HOME:
+    //! Получаем ссылку на кнопку HOME:
     homeBtn: document.querySelector('.button-home'),
 
-    //todo Получаем ссылку на кнопку Filmoteka:
+    //! Получаем ссылку на кнопку Filmoteka:
     filmotekaBtn: document.querySelector('.button-filmoteka'),
 
-    //todo Получаем ссылку на строку предупреждения об отсутствии фильмов:
-    resultNotSuccessful: document.querySelector('.search-form-alert'),
-
-    //todo Получаем ссылку на <section class="section-hero">:
+    //! Получаем ссылку на <section class="section-hero"> ==> на poster_path:
     movieDetails: document.querySelector('.section-hero'),
+
+    //! Получаем ссылки для модалки:
+    // openModalBtn: document.querySelector('[data-action="open-modal"]'), //! ----- для тестирования
+    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+    backdrop: document.querySelector('.js-backdrop'),
+
+    //! Получаем ссылку на строку предупреждения об отсутствии фильмов:
+    resultNotSuccessful: document.querySelector('.search-form-alert'),
   };
-}
+};
 
